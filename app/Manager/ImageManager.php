a888d3c0401d8ebf1861442621de5070c0dcdd2e
seboccis <seboccis@orange.fr>
2015-11-10 12:29:15 +0100

    add ImageManager

---
+++ app/Manager/ImageManager.php
@@ -0,0 +1,35 @@
+<?php
+
+	namespace Manager;
+
+	use Manager\DefaultManager;
+
+	/**
+	 * Le manager d'images
+	 */
+	class ImageManager extends DefaultManager
+	{
+
+		/**
+		 * Cherche le nom de l'image principale d'un véhicule
+		 * @param int $id Id du véhicule
+		 * @return string Le nom du fichier image
+		 */
+		public function findCarPrincipalImgFileName($id)
+		{
+			$sql = "SELECT fileName
+					FROM " . $this->table . "
+					WHERE id = 	(
+									SELECT id_img
+									FROM images_cars
+									WHERE id_car = :id
+									AND isPrincipal = 1
+								)";
+			$sth = $this->dbh->prepare($sql);
+			$sth->bindValue(":id", $id);
+			$sth->execute();
+
+			return $sth->fetchColumn();
+		}
+
+	}
